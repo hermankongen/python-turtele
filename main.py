928fea64b1395ce728cd08e440bd73e21fe03b42
mr.ducky <37117219+hermankongen@users.noreply.github.com>
2021-07-08 02:39:56 +0200

    Update main.py
    updated

--- main.py
+++ main.py
@@ -2,16 +2,10 @@ import random
 
 import turtle 
 
-kon = turtle.Turtle()
-h =  turtle.Turtle()
-colours=["red", "black", "white","yellow","cyan", ]
-
-
-
+hel = turtle.Turtle()
+colours=["red", "black", "white","yellow","cyan"]
 
 for g in range(169):  # antal repetasjoner
-
-  #color=random.choice(colour)
   lenght=random.randint(-100,100,)
   vestre=random.randint(1,359) 
   colour = random.choice(colours)  # Henter frarver fra colors variabelen til turtle.
